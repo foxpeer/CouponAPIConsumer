d2313e28b77820eddf7383ad202b469d2c8afe64
Lenard_glb <xinglonglu565@gmail.com>
2020-11-23 21:16:47 -0500

    Coupon API Consumer created

---
+++ APIConsumer/MainWindow.xaml.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net.Http;
+using Newtonsoft.Json;
+
+namespace APIConsumer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private static readonly HttpClient client = new HttpClient();
+
+        private static string url = "https://foxpeer-eval-test.apigee.net/api/coupons";
+
+    
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn_get_Click(object sender, RoutedEventArgs e)
+        {
+            HttpResponseMessage responseMessage = client.GetAsync(url).Result;
+            Coupon[] resultList;
+            string jsonResponse = responseMessage.Content.ReadAsStringAsync().Result;
+            resultList = JsonConvert.DeserializeObject<Coupon[]>(jsonResponse);
+            gridDisplayCoupon.ItemsSource = resultList;
+        }
+
+        private void Btn_post_Click(object sender, RoutedEventArgs e)
+        {
+            PostCoupon postCoupon = new PostCoupon();
+            postCoupon.Show();
+        }
+
+        private void Btn_put_Click(object sender, RoutedEventArgs e)
+        {
+            UpdateCoupon updateCoupon = new UpdateCoupon();
+            updateCoupon.Show();
+        }
+
+        private void Btn_delete_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Coupon deleteCoupon = new Coupon();
+                deleteCoupon.Id = textCouponID.Text;
+                if(textCouponID.Text == "")
+                {
+                    throw new Exception("Please enter coupon ID!");
+                }
+                if (MessageBox.Show("Do you want to delete this coupon?", "Warning", MessageBoxButton.OKCancel,
+               MessageBoxImage.Warning) == MessageBoxResult.OK)
+                {                    
+                    var deleteResult = client.DeleteAsync(url + "/" + deleteCoupon.Id.ToString()).Result;
+                    if (deleteResult.StatusCode == System.Net.HttpStatusCode.NotFound || deleteResult.StatusCode == System.Net.HttpStatusCode.MethodNotAllowed)
+                    {
+                        throw new Exception("Please enter correct coupon ID!");
+                    }
+                    MessageBox.Show(deleteResult.ToString());
+                }
+                else
+                {
+                    return;
+                }
+            }
+           catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Warning", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+        }
+
+        private void Btn_getByID_Click(object sender, RoutedEventArgs e)
+        {
+            HttpResponseMessage getByIDResult = client.GetAsync(url+"/"+textCouponID.Text).Result;
+            Coupon gettedCouponByID;
+            string jsonResponse = getByIDResult.Content.ReadAsStringAsync().Result;
+            gettedCouponByID= JsonConvert.DeserializeObject<Coupon>(jsonResponse);
+
+            List<Coupon> getCouponResult = new List<Coupon>();
+            getCouponResult.Add(gettedCouponByID);
+            gridDisplayCoupon.ItemsSource = getCouponResult;
+        }
+    }
+}
---
+++ APIConsumer/PostCoupon.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Net.Http;
+using Newtonsoft.Json;
+
+namespace APIConsumer
+{
+    /// <summary>
+    /// Interaction logic for PostCoupon.xaml
+    /// </summary>
+    public partial class PostCoupon : Window
+    {
+        private static readonly HttpClient client = new HttpClient();
+
+        private static string url = "https://foxpeer-eval-test.apigee.net/api/coupons";
+        public PostCoupon()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSubmit_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Coupon newCoupon = new Coupon();
+                newCoupon.CouponCode = textCouponCode.Text;
+                newCoupon.StoreName = textStoreName.Text;
+                newCoupon.ItemName = textItemName.Text;
+
+                if (textCouponCode.Text == "" || textStoreName.Text == "" || textItemName.Text == "")
+                {
+                    throw new Exception("Please fill all blanks");
+                }
+                string newCouponJsonString = JsonConvert.SerializeObject(newCoupon);
+                var newCouponToPost = new StringContent(newCouponJsonString, Encoding.UTF8, "application/json");
+                var postResult = client.PostAsync(url, newCouponToPost).Result;
+                MessageBox.Show(postResult.ToString());
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Warning", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+           
+        }
+
+        private void btnCancle_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
